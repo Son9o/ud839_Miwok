766788d5192eb7e831b26348f3daabc05f3ca908
son9o <songopll@gmail.com>
2019-03-26 10:31:13 +0100

    before miedia player

--- app/src/main/java/com/example/android/miwok/NumbersActivity2.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity2.java
@@ -9,5 +9,8 @@ public class NumbersActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers2);
+
+
+        
     }
 }
